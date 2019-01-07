28eff78172213e6ed98e14b3f049b836bfdb17ab
leminhyen2 <leming16@apu.ac.jp>
2019-01-07 16:11:35 +0900

    Updates

---
+++ precache-manifest.a956b30c191e721abccfd8eece5c5f6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8622bb1c014a01545b7e",
+    "url": "/ReactJs-Food-Ordering-Form/static/js/runtime~main.8622bb1c.js"
+  },
+  {
+    "revision": "f621eb72fe4b3942c459",
+    "url": "/ReactJs-Food-Ordering-Form/static/js/main.f621eb72.chunk.js"
+  },
+  {
+    "revision": "28677c3cf74fbe0a2839",
+    "url": "/ReactJs-Food-Ordering-Form/static/js/1.28677c3c.chunk.js"
+  },
+  {
+    "revision": "f621eb72fe4b3942c459",
+    "url": "/ReactJs-Food-Ordering-Form/static/css/main.307cdf3a.chunk.css"
+  },
+  {
+    "revision": "0f7edcff2ef64f8ebf8d7bedbd2d9ab0",
+    "url": "/ReactJs-Food-Ordering-Form/index.html"
+  }
+];
